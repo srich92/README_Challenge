05197ba8dc4fea4942f95c0e1e501ec58dd1b36e
srich92 <srichards646@gmail.com>
2022-11-09 17:18:06 -0500

    functions created to write README

--- README_GenChallenge/utils/generateMarkdown.js
+++ README_GenChallenge/utils/generateMarkdown.js
@@ -1,9 +1,6 @@
-
-const fs = require('fs')
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  
+function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -11,19 +8,13 @@ function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {
-  if (!license) {
-    return ``;
-  } else {
-    return `[![${license} license](https://img.shields.io/badge/License-${license}-blue.svg)](${renderLicenseLink(license)})`
-  }
-}
+function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
+
 }
 
 module.exports = generateMarkdown;
